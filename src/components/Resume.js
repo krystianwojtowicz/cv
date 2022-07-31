3fe87b25411d53303c0ec5a0b80e037330f73972
Krystian <krystian1wojtowicz@gmail.com>
2022-07-31 13:25:43 +0200

    after adding pdf

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,14 +2,21 @@ import React, { Component } from "react";
 import Personal from "./Personal";
 import Experience from "./Experience";
 import Education from "./Education";
+import Pdf from "react-to-pdf";
+const ref = React.createRef();
 
 class Resume extends Component {
   render() {
     return (
       <>
-        {/* <Personal></Personal>
+        <div ref={ref}>
+          {/* <Personal></Personal>
         <Experience></Experience> */}
-        <Education></Education>
+          <Education></Education>
+        </div>
+        <Pdf targetRef={ref} filename="post.pdf">
+          {({ toPdf }) => <button onClick={toPdf}>Generate PDF</button>}
+        </Pdf>
       </>
     );
   }
