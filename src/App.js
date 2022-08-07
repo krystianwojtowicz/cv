8bbdabd7ee536d519b39fbbfdae0a5774705c56d
Krystian <krystian1wojtowicz@gmail.com>
2022-08-07 08:44:39 +0200

    printing is fine

--- src/App.js
+++ src/App.js
@@ -1,43 +1,18 @@
 import React, { Component } from "react";
 import Resume from "./components/Resume";
 import Instructions from "./components/Instructions";
-import { PDFDownloadLink } from "@react-pdf/renderer";
-import ResumeToPrint from "./components/ResumeToPrint";
 import "./App.css";
-import { PDFViewer } from "@react-pdf/renderer";
 
 class App extends Component {
   render() {
     return (
       <>
-        <PDFDownloadLink Document={<ResumeToPrint />} FileName="somename.pdf">
-          {({ Blob, Url, Loading, Error }) =>
-            Loading ? "Loading Document..." : "Download Now!"
-          }
-        </PDFDownloadLink>
-        {/* <PDFDownloadLink document={<ResumeToPrint />} fileName="Form ">
-          {({ loading }) =>
-            loading ? (
-              <button>Loading Document...</button>
-            ) : (
-              <button>Download</button>
-            )
-          }
-        </PDFDownloadLink> */}
-        {/* <ResumeToPrint></ResumeToPrint> */}
-
-        {/* <PDFDownloadLink document={<ResumeToPrint />} fileName="movielist.pdf">
-          {({ blob, url, loading, error }) =>
-            loading ? "Loading document..." : "Download Pdf"
-          }
-        </PDFDownloadLink> */}
-
         <Instructions></Instructions>
         <Resume />
+        <div className="divFooter">footer</div>
       </>
     );
   }
 }
 
 export default App;
-// ReactPDF.render(<ResumeToPrint />, `/example.pdf`);
