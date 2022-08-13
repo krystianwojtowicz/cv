fa7fe7cb011fabf741dbb41805561590f0ab14f3
Krystian <krystian1wojtowicz@gmail.com>
2022-08-13 09:01:46 +0200

    changing personal and skills in function components

--- src/components/Personal.js
+++ src/components/Personal.js
@@ -1,72 +1,65 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import img from "./Krystian.jpg";
 
-class Personal extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      fullName: "",
-      phone: "",
-      email: "",
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
-  handleChange(event) {
-    this.setState({
-      // Computed property names
-      // keys of the objects are computed dynamically
-      [event.target.name]: event.target.value,
-    });
-  }
-  onSubmitTask(e) {
+function Personal() {
+  const [fullName, setFullName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+
+  const onSubmitTask = (e) => {
     e.preventDefault();
     e.currentTarget.classList.remove("visible");
-  }
-  handleClick(e) {
+  };
+
+  const handleClick = (e) => {
     e.currentTarget.nextElementSibling.classList.add("visible");
-  }
-  render() {
-    const { fullName, phone, email } = this.state;
-    return (
-      <div id="Personal">
-        <h2>{fullName ? fullName : "Krystian Wójtowicz"}</h2>
-        <img src={img || null} alt="" />
-        <label htmlFor="phone">Telefon</label>
-        <h2>{phone ? phone : "789194734"}</h2>
-        <label htmlFor="email">E-mail</label>
-        <h4>{email ? email : "krystian1wojtowicz@gmail.com"}</h4>
-        <button onClick={this.handleClick} className="to-edit">
-          edit
-        </button>
-        <form className="form" onSubmit={this.onSubmitTask}>
-          <div>
-            <input
-              onChange={this.handleChange}
-              value={this.state.fullName}
-              type="text"
-              placeholder="Full name"
-              name="fullName"
-            />
-            <input
-              onChange={this.handleChange}
-              value={this.state.phone}
-              type="text"
-              placeholder="phone"
-              name="phone"
-            />
-            <input
-              onChange={this.handleChange}
-              value={this.state.email}
-              type="text"
-              placeholder="email"
-              name="email"
-            />
-          </div>
-          <button type="submit">submit</button>
-        </form>
-      </div>
-    );
-  }
+  };
+
+  return (
+    <div id="Personal">
+      <h2>{fullName ? fullName : "Krystian Wójtowicz"}</h2>
+      <img src={img} alt="" />
+      <label htmlFor="phone">Telefon</label>
+      <h2>{phone ? phone : "789194734"}</h2>
+      <label htmlFor="email">E-mail</label>
+      <h4>{email ? email : "krystian1wojtowicz@gmail.com"}</h4>
+      <button onClick={handleClick} className="to-edit">
+        edit
+      </button>
+      <form className="form" onSubmit={onSubmitTask}>
+        <div>
+          <input
+            onChange={(e) => {
+              setFullName(e.target.value);
+            }}
+            value={fullName}
+            type="text"
+            placeholder="Full name"
+            name="fullName"
+          />
+          <input
+            onChange={(e) => {
+              setPhone(e.target.value);
+            }}
+            value={phone}
+            type="text"
+            placeholder="phone"
+            name="phone"
+          />
+          <input
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            value={email}
+            type="text"
+            placeholder="email"
+            name="email"
+          />
+        </div>
+        <button type="submit">submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default Personal;
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,85 +1,73 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import uniqid from "uniqid";
 
-class Skills extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      id: uniqid(),
-      skill: "",
-      skills: [
-        { id: uniqid(), skill: "SQL" },
-        { id: uniqid(), skill: "Typescript" },
-        { id: uniqid(), skill: "React" },
-        { id: uniqid(), skill: "JavaScript" },
-        { id: uniqid(), skill: "HTML5" },
-        { id: uniqid(), skill: "CSS3" },
-        { id: uniqid(), skill: "Git" },
-        { id: uniqid(), skill: "Sass" },
-        { id: uniqid(), skill: "Bootstrap" },
-        { id: uniqid(), skill: "jQuery" },
-        { id: uniqid(), skill: "BEM" },
-        { id: uniqid(), skill: "SCSS" },
-        { id: uniqid(), skill: "Testy integracyjne i jednostkowe" },
-        { id: uniqid(), skill: "WCAG" },
-        { id: uniqid(), skill: "Jest" },
-        { id: uniqid(), skill: "Webpack" },
-      ],
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-  }
-  handleChange(event) {
-    this.setState({
-      // Computed property names
-      // keys of the objects are computed dynamically
-      [event.target.name]: event.target.value,
-    });
-  }
-  handleClick(e) {
+function Skills() {
+  const initialState = [
+    { id: uniqid(), name: "SQL" },
+    { id: uniqid(), name: "Typescript" },
+    { id: uniqid(), name: "React" },
+    { id: uniqid(), name: "JavaScript" },
+    { id: uniqid(), name: "HTML5" },
+    { id: uniqid(), name: "CSS3" },
+    { id: uniqid(), name: "Git" },
+    { id: uniqid(), name: "Sass" },
+    { id: uniqid(), name: "Bootstrap" },
+    { id: uniqid(), name: "jQuery" },
+    { id: uniqid(), name: "BEM" },
+    { id: uniqid(), name: "Testy integracyjne i jednostkowe" },
+    { id: uniqid(), name: "WCAG" },
+    { id: uniqid(), name: "Jest" },
+    { id: uniqid(), name: "Webpack" },
+  ];
+
+  const [skills, setSkills] = useState(initialState);
+  const [newSkill, setNewSkill] = useState("");
+
+  const add = (skill) => setSkills([...skills, skill]);
+
+  const handleClick = (e) => {
     e.currentTarget.parentElement.classList.remove("visible");
-    console.log("dziala");
-    const { skill, skills } = this.state;
-    const skillNew = {
-      id: uniqid(),
-      skill,
-    };
-    this.setState((prevState) => ({
-      skill: "",
-      skills: [...prevState.skills, skillNew],
-    }));
-  }
-  handleAdd(e) {
+    add({ name: newSkill, id: uniqid() });
+    setNewSkill("");
+  };
+
+  const handleAdd = (e) => {
     e.currentTarget.nextElementSibling.classList.add("visible");
-  }
-  render() {
-    const { skill, skills } = this.state;
-    return (
-      <div id="Skills">
-        <p>Umiejętności: </p>
-        <ul className="skills">
-          {skills.map((skil) => {
-            return <li key={skil.id}>{skil.skill}</li>;
-          })}
-        </ul>
-        <button onClick={this.handleAdd} className="adding-skills">
-          Dodaj
-        </button>
-        <div className="add-skills">
-          <div>
-            <input
-              onChange={this.handleChange}
-              value={this.state.skill}
-              type="text"
-              placeholder="add skill"
-              name="skill"
-            />
-          </div>
-          <button onClick={this.handleClick}>Dodaj</button>
+  };
+
+  return (
+    <div id="Skills">
+      <p>Umiejętności: </p>
+      <ul className="skills">
+        {skills.map((skill, index) => {
+          return <Skill key={skill.id} skill={skill} />;
+        })}
+      </ul>
+      <button onClick={handleAdd} className="adding-skills">
+        Dodaj
+      </button>
+      <div className="add-skills">
+        <div>
+          <input
+            onChange={(event) => setNewSkill(event.target.value)}
+            value={newSkill}
+            type="text"
+            placeholder="add skill"
+            name="skill"
+          />
         </div>
+        <button onClick={handleClick}>Dodaj</button>
       </div>
-    );
-  }
+    </div>
+  );
+}
+
+function Skill({ skill }) {
+  return (
+    <li className="skill">
+      <span>{skill.name}</span>
+    </li>
+  );
 }
 
 export default Skills;
